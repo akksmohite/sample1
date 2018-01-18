bdb641c2460c4957e53cc2e742c8ebe48f1caae0
Akshay Mohite <akshaym6@kpit.com>
2018-01-18 12:02:05 +0530

    added new line

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
 
 		            System.out.println("Hello akshay man");
 					System.out.println("*********************************");
+					System.out.println("*********************************");
 			        }
 }
